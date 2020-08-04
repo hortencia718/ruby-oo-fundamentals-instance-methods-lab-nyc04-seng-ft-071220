065aec600e85c593f6af17e24711f729f5dc1605
hortencia c <hortenciaecisneros@gmail.com>
2020-08-04 22:40:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,3 +10,4 @@ class Dog
 end
 
 # define a "class "Dog make sure its in caps ! and make it do something like "bark " ! and create a realistic thing a dog would do like say "Woof" ! 
+# add each seprately like so "def" bark def sit and end end on each !
